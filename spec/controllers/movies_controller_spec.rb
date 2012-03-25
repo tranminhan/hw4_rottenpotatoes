2dd382af0f6a8d746727439778152265ac778fd8
An Tran <tranminhan@hotmail.com>
2012-03-25 14:46:03 +0700

    refactor

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -9,15 +9,20 @@ describe MoviesController do
         Movie.should_receive(:find_in_tmdb).with("hardware").and_return(@fake_results)
         post :search_tmdb, { :search_terms => "hardware" }
     end
-    it 'should select the Search Results template for rendering' do
-        Movie.stub(:find_in_tmdb).with("hardware").and_return(@fake_results)
-        post :search_tmdb, { :search_terms => "hardware" }
-        response.should render_template('search_tmdb')
-    end
-    it 'should make the TMDb search results available to that template' do
-        Movie.stub(:find_in_tmdb).with("hardware").and_return(@fake_results)
-        post :search_tmdb, { :search_terms => "hardware" }
-        assigns(:movies).should == @fake_results
+
+    describe 'after valid search' do 
+        before :each do 
+            Movie.stub(:find_in_tmdb).with("hardware").and_return(@fake_results)
+            post :search_tmdb, { :search_terms => "hardware" }
+        end 
+
+        it 'should select the Search Results template for rendering' do
+            response.should render_template('search_tmdb')
+        end
+        it 'should make the TMDb search results available to that template' do
+            assigns(:movies).should == @fake_results
+        end 
     end 
+
   end
 end
