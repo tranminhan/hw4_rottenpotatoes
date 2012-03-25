a461ec7ede51cf8a922a826b3dd61036d3255a36
An Tran <tranminhan@hotmail.com>
2012-03-26 00:54:22 +0700

    fix path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,9 +15,9 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
-    when /^the edit page for (.*)$/
+    when /^the edit page for "(.*)"$/
       edit_movie_path(Movie.find_by_title($1))
-    when /^the details page for (.*)$/
+    when /^the details page for "(.*)"$/
       movie_path(Movie.find_by_title($1))
 
     # Add more mappings here.
