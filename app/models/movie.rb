7dc9babcaaae0ea99e3af6c1c393c2306ebdf221
An Tran <tranminhan@hotmail.com>
2012-03-25 16:57:38 +0700

    add model spec and custom error

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,20 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
-  end
+    
+    class Movie::InvalidKeyError < StandardError ; end
+
+    def self.api_key
+        ''
+    end 
+
+    def self.all_ratings
+        %w(G PG PG-13 NC-17 R)
+    end
+
+    def self.find_in_tmdb(string) 
+        begin 
+            TmdbMovie.find(:title => string)        
+        rescue ArgumentError => tmdb_error
+            raise Movie::InvalidKeyError, tmdb_error.message 
+        end  
+    end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,16 @@
 # spec/models/movie_spec.rb:
+require 'spec_helper'
 
 describe Movie do
-    
+    describe 'search Tmdb by keyword' do
+        it 'should call Tmdb with title keywords given valid API key' do
+            TmdbMovie.should_receive(:find).with(hash_including :title => 'Inception')
+            Movie.find_in_tmdb('Inception')
+        end 
+
+        it 'should raise an InvalidKeyError with no API key' do
+            lambda { Movie.find_in_tmdb('Inception') }.
+            should raise_error(Movie::InvalidKeyError)
+        end 
+    end 
 end
